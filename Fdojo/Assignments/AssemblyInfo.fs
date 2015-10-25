a880319fd2cc62c0ac490762122616416acdb4f7
Anssi Heiska <aheiska@iki.fi>
2015-10-25 22:30:46 +0200

    Loput F# kamat.

---
+++ Fdojo/Assignments/AssemblyInfo.fs
@@ -0,0 +1,20 @@
+﻿namespace Assignments
+open System.Reflection
+open System.Runtime.CompilerServices
+
+[<assembly: AssemblyTitle("Assignments")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("")>]
+[<assembly: AssemblyCopyright("anssi")>]
+[<assembly: AssemblyTrademark("")>]
+
+// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+
+[<assembly: AssemblyVersion("1.0.0.0")>]
+
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
+
+()
---
+++ Fdojo/Assignments/Reduce.fs
@@ -0,0 +1,32 @@
+﻿namespace Assignments
+
+module Reduce =
+
+    let rec sum xs =
+        match xs with
+        | []    -> 0
+        | y::ys -> y + sum ys
+
+    let rec mult xs =
+        match xs with
+        | [] -> 1
+        | y :: ys -> y * mult ys
+
+    let rec lens xs =
+        match xs with
+        | [] -> 0
+        | s :: ss -> String.length(s) + lens ss
+
+    let double x = x+x
+
+    let rec sumDoubles xs =
+        match xs with
+        | [] -> 0
+        | y :: ys -> double y + sumDoubles ys
+
+    let rec reduce f xs =
+        match xs with
+        | [] -> invalidArg "reduce for empty list"
+        | y :: [] -> y
+        | y :: ys -> f y (reduce f ys)
+
